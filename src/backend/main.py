91d487e0fe4748e5cc1646e1734dc33da6c50947
pablaofficeal <pavlogorosko56@gmail.com>
2026-02-17 22:01:01 +0100

    feat: Set up a basic Flask "hello world" endpoint and corresponding HTML template.

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from config import Config
+from routers.home.main_home import main_home_bpp
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../../src/frontend/templates', static_folder='../../src/frontend/static')
 app.config.from_object(Config)
+
+app.register_blueprint(main_home_bpp)
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'], host=app.config['HOST'], port=app.config['PORT'])
--- src/backend/routers/home/main_home.py
+++ src/backend/routers/home/main_home.py
@@ -1,1 +1,7 @@
 from flask import Blueprint, render_template
+
+main_home_bpp = Blueprint('main_home_bpp', __name__)
+
+@main_home_bpp.route('/')
+def index():
+    return render_template('index.html')
